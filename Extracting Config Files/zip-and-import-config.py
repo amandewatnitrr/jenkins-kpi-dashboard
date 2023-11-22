c3cd664820580d54b8451e86a533b549d2db6850
amandewatnitrr <amandewatnitrr@gmail.com>
2023-11-22 14:27:17 +0530

    Working on Algo to Zip and Import Config files

--- Extracting Config Files/zip-and-import-config.py
+++ Extracting Config Files/zip-and-import-config.py
@@ -1,27 +1,28 @@
-import os
 import paramiko
-import tarfile
+import os
 
-def zip_and_download_jobs(host, port, username, password, remote_path, local_path):
+def zip_and_download_jobs_folder(host, port, username, password, remote_path, local_path):
     transport = paramiko.Transport((host, port))
     transport.connect(username=username, password=password)
     sftp = paramiko.SFTPClient.from_transport(transport)
 
-    # Create a temporary compressed archive on the remote server
-    remote_archive_path = f"{remote_path.rstrip('/')}_archive.tar.gz"
-    local_archive_path = os.path.join(local_path, "jobs_archive.tar.gz")
+    # Remote command to zip the jobs folder
+    zip_command = f"cd {remote_path} && zip -r - jobs > jobs.zip"
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, port=port, username=username, password=password)
+    ssh.exec_command(zip_command)
+    ssh.close()
 
-    with sftp.file(remote_archive_path, 'wb') as remote_archive:
-        # Use tar to create a compressed archive of the jobs folder
-        command = f"tar czf - -C {remote_path} jobs"
-        stdin, stdout, stderr = sftp.exec_command(command)
-        remote_archive.write(stdout.read())
+    # Download the zipped file
+    remote_zip_path = os.path.join(remote_path, 'jobs.zip')
+    local_zip_path = os.path.join(local_path, 'jobs.zip')
+    sftp.get(remote_zip_path, local_zip_path)
 
-    # Download the compressed archive to the local system
-    sftp.get(remote_archive_path, local_archive_path)
+    print(f"ZIP file downloaded to: {local_zip_path}")
 
-    # Cleanup: Remove the temporary compressed archive on the remote server
-    sftp.remove(remote_archive_path)
+    # Cleanup: Remove the remote zipped file
+    sftp.remove(remote_zip_path)
 
     sftp.close()
     transport.close()
@@ -31,7 +32,8 @@ remote_ip = 'XX.XX.XX.XX'
 remote_port = 22
 remote_path = "/home/jenkins/CORELOAD-INBLRJENKINS01/jenkins_home"
 local_path = r"C:\Users\YourUsername\Documents"
+
 username = 'your_username'
 password = 'your_password'
 
-zip_and_download_jobs(remote_ip, remote_port, username, password, remote_path, local_path)
+zip_and_download_jobs_folder(remote_ip, remote_port, username, password, remote_path, local_path)
