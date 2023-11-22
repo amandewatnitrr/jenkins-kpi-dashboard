975b6c85d3bb8dd90dd1401ba250bd90a3986abc
amandewatnitrr <amandewatnitrr@gmail.com>
2023-11-22 12:09:44 +0530

    Working on Algo to Zip and Import Config files

--- Extracting Config Files/zip-and-import-config.py
+++ Extracting Config Files/zip-and-import-config.py
@@ -1,34 +1,37 @@
 import os
-import zipfile
 import paramiko
+import tarfile
 
-def zip_and_download(remote_ip, remote_path, local_path):
-    # Connect to the remote server using SSH
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    
-    # Replace 'username' and 'password' with your actual credentials
-    ssh.connect(remote_ip, username='username', password='password')
+def zip_and_download_jobs(host, port, username, password, remote_path, local_path):
+    transport = paramiko.Transport((host, port))
+    transport.connect(username=username, password=password)
+    sftp = paramiko.SFTPClient.from_transport(transport)
 
-    try:
-        # Create a temporary zip file on the remote server
-        zip_filename = '/tmp/jobs.zip'
-        ssh.exec_command(f'cd {remote_path} && zip -r {zip_filename} jobs')
+    # Create a temporary compressed archive on the remote server
+    remote_archive_path = f"{remote_path.rstrip('/')}_archive.tar.gz"
+    local_archive_path = os.path.join(local_path, "jobs_archive.tar.gz")
 
-        # Download the zip file
-        transport = paramiko.Transport((remote_ip, 22))
-        transport.connect(username='username', password='password')
-        sftp = paramiko.SFTPClient.from_transport(transport)
-        sftp.get(zip_filename, os.path.join(local_path, 'jobs.zip'))
-        sftp.close()
+    with sftp.file(remote_archive_path, 'wb') as remote_archive:
+        # Use tar to create a compressed archive of the jobs folder
+        command = f"tar czf - -C {remote_path} jobs"
+        stdin, stdout, stderr = sftp.exec_command(command)
+        remote_archive.write(stdout.read())
 
-    finally:
-        # Close the SSH connection
-        ssh.close()
+    # Download the compressed archive to the local system
+    sftp.get(remote_archive_path, local_archive_path)
 
-# Example usage
+    # Cleanup: Remove the temporary compressed archive on the remote server
+    sftp.remove(remote_archive_path)
+
+    sftp.close()
+    transport.close()
+
+# Replace these values with your actual credentials and paths
 remote_ip = 'XX.XX.XX.XX'
-remote_path = '/home/jenkins/CORELOAD-INBLRJENKINS01/jenkins_home/'
-local_path = 'C:/Users/YourUsername/Documents/'
+remote_port = 22
+remote_path = "/home/jenkins/CORELOAD-INBLRJENKINS01/jenkins_home"
+local_path = r"C:\Users\YourUsername\Documents"
+username = 'your_username'
+password = 'your_password'
 
-zip_and_download(remote_ip, remote_path, local_path)
+zip_and_download_jobs(remote_ip, remote_port, username, password, remote_path, local_path)
