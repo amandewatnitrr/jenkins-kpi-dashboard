05aed1ed6959801b1b25227eee6843bfeef01b42
amandewatnitrr <amandewatnitrr@gmail.com>
2023-12-24 20:30:19 +0530

    Jenkins API Call Trial

--- kpi_dashboard/jenkins_servers/views.py
+++ kpi_dashboard/jenkins_servers/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render
-from .models import jenkins_servers  # Import the model
+from .models import jenkins_servers
+import jenkins
+import requests
+
+def get_jenkins_server_details(server_url, username, password):
+    # Create a Jenkins server instance
+    server = jenkins.Jenkins(server_url, username=username, password=password)
+
+    # Get server details using the Jenkins API
+    server_info = server.get_info()
+
+    # Extract relevant information
+    server_up_time = server_info.get('mode', 'N/A')
+    response_time = server_info.get('quietingDown', 'N/A')
+    network_latency = server_info.get('useSecurity', 'N/A')  # You may need to adjust this field
+    status = 'Working' if server_info.get('mode', 'NORMAL') == 'Working' else 'Failure'
+
+    return server_up_time, response_time, network_latency, status
+
+def update_jenkins_server_details():
+    # Fetch all Jenkins servers from the database
+    jenkins_servers_list = jenkins_servers.objects.all()
+
+    for server in jenkins_servers_list:
+        # Get Jenkins server details from the API
+        server_up_time, response_time, network_latency, status = get_jenkins_server_details(
+            server.server_url,
+            'your_jenkins_username',
+            'your_jenkins_password'
+        )
+
+        # Update the database with the obtained details
+        server.server_up_time = server_up_time
+        server.response_time = response_time
+        server.network_latency = network_latency
+        server.status = status
+        server.save()
 
 def jenkins_servers_view(request):
+    # Update Jenkins server details in the database
+    update_jenkins_server_details()
+
     # Fetch data from the model
     queryset = jenkins_servers.objects.all()
 
