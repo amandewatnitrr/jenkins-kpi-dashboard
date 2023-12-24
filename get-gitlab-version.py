e44d00218a61a0fb2e56f1696ee278ccbbd1b033
amandewatnitrr <amandewatnitrr@gmail.com>
2023-12-24 21:05:43 +0530

    Gitlab API Call Trial

--- get-gitlab-version.py
+++ get-gitlab-version.py
@@ -1,35 +1,27 @@
 import requests
 
-def get_gitlab_version(url, username, password):
-    # Disable SSL certificate verification (not recommended for production)
-    requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
-
-    # Make a request to the GitLab API to fetch version details
+def get_gitlab_version(url, private_token):
     try:
         response = requests.get(
-            f"{url.rstrip('/')}/version",
-            auth=(username, password),
-            timeout=10,
-            verify=False  # Disable SSL verification
+            f"{url.rstrip('/')}/api/v4/version",
+            headers={'Private-Token': private_token},
+            timeout=10
         )
         response.raise_for_status()
         gitlab_info = response.json()
     except requests.RequestException as e:
-        # Handle the exception (e.g., log the error)
         print(f"Error retrieving GitLab version: {e}")
         return 'N/A'
 
-    # Extract the GitLab version
     version = gitlab_info.get('version', 'N/A')
     return version
 
-# Replace these values with your GitLab server URL, username, and password
+# Replace these values with your GitLab server URL and Private Access Token
 gitlab_url = 'your_gitlab_url'
-gitlab_username = 'your_username'
-gitlab_password = 'your_password'
+private_token = 'your_private_access_token'
 
 # Get GitLab version
-gitlab_version = get_gitlab_version(gitlab_url, gitlab_username, gitlab_password)
+gitlab_version = get_gitlab_version(gitlab_url, private_token)
 
 # Print the GitLab version
 print(f"GitLab version: {gitlab_version}")
